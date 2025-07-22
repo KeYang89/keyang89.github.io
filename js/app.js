85352af852361641023e015e777e153993b656ae
Ke Yang <yangkecoy@gmail.com>
2025-07-22 17:36:41 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,18 +14,8 @@ angular.module('raw', [
   'ngFileUpload'
 ])
 
-.config(['$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
-
-  // Disable HTML5 mode to keep hashbang URLs (like #/patents)
-  $locationProvider.html5Mode(false);
-
-  // Route definitions
-  $routeProvider
-    .when('/', {
-      templateUrl: 'partials/main.html',
-      controller: 'RawCtrl'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
+.config(['$routeProvider','$locationProvider', function ($routeProvider,$locationProvider) {
+  $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'RawCtrl'});
+  $routeProvider.otherwise({redirectTo: '/'});
+  $locationProvider.html5Mode(true);
 }]);
